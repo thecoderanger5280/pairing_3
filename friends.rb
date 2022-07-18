6c38faa2a1b1e3fc0bfbe436657df07f43dd6ca1
Ryan Loughry <thecoderanger5280@icloud.com>
2022-07-18 15:51:43 -0600

    Added logic for another name

--- friends.rb
+++ friends.rb
@@ -1,9 +1,10 @@
-best_friend = "Mike"
+best_friend = "Max"
+best_friend2 = "Bob"
 
 if best_friend == "Max"
   puts "This is my best friend Max"
-elsif best_friend != "Max"
-  puts "This is not max"
+elsif best_friend2 == "Bob"
+  puts "This is Bob"
 else
-  puts "Please try again"
+  puts "This is not Bob or Max"
 end
